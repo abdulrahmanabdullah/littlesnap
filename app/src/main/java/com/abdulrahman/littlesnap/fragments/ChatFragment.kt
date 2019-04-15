4fce5b3ec7368e1b6d57ba0bf70c8b85843fb576
abdulrahmanabdullah <nfs056@gmail.com>
2019-04-15 12:02:27 +0300

    downloads avatars photo and create ChatAdapter

--- app/src/main/java/com/abdulrahman/littlesnap/fragments/ChatFragment.kt
+++ app/src/main/java/com/abdulrahman/littlesnap/fragments/ChatFragment.kt
@@ -2,12 +2,15 @@ package com.abdulrahman.littlesnap.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.abdulrahman.littlesnap.R
 
 class ChatFragment  : BaseFragment(){
+
+
     override fun inOnCreateView(view: View, container: ViewGroup?, bundle: Bundle?) {
 
     }
@@ -20,5 +23,27 @@ class ChatFragment  : BaseFragment(){
 
     override fun getLayoutResId(): Int {
         return R.layout.fragment_chat
+    }
+}
+
+//Adapter
+class ChatAdapter : RecyclerView.Adapter<ChatAdapter.ChatHolder>(){
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ChatHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ChatHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    //Holder ..
+     class ChatHolder(val view:View) : RecyclerView.ViewHolder(view){
+
     }
 }
